b30a7ecc6ac6a3381405f0a7490177d3ba3ea22c
Alcohol Based Non Toxic <alcoholbasednontoxic@gmail.com>
2025-09-02 11:15:45 +0000

    setup of circles and queues could made after "with disjointed chains" de

--- src/lib/solver-multi-chain.test.ts
+++ src/lib/solver-multi-chain.test.ts
@@ -13,20 +13,25 @@ describe('solveMultiChain', () => {
   });
 
   describe('with disjointed chains', () => {
-    it('should solve for two separate chains, "MASTAND" and "SPEEDGEAR"', () => {
-        const circles: Circles = {};
-        const queues: Queues = {
-          chain1: ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6'], // MASTAND
-          chain2: ['d0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'], // SPEEDGEAR
-        };
-        Object.keys(queues).forEach(key => {
-            queues[key].forEach((id, i) => {
-                if (!circles[id]) {
-                    circles[id] = { x: i * 10, y: (key === 'chain1' ? 0 : 20) };
-                }
-            });
-        });
+    let circles: Circles;
+    let queues: Queues;
 
+    beforeEach(() => {
+      circles = {};
+      queues = {
+        chain1: ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6'], // MASTAND
+        chain2: ['d0', 'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8'], // SPEEDGEAR
+      };
+      Object.keys(queues).forEach(key => {
+          queues[key].forEach((id, i) => {
+              if (!circles[id]) {
+                  circles[id] = { x: i * 10, y: (key === 'chain1' ? 0 : 20) };
+              }
+          });
+      });
+    });
+
+    it('should solve for two separate chains, "MASTAND" and "SPEEDGEAR"', () => {
         const result = solveMultiChain(queues, circles, words, connections);
 
         expect(result.solution).not.toBeNull();
